61915535fd4a6ffcfc2030a09631fb3b9770f043
Christian Wiegand <cwiegand@wgdnet.de>
2020-12-06 11:15:56 +0100

    day 6

---
+++ day7/day7.py
@@ -0,0 +1,8 @@
+def solve_part1():
+    with open('day7/answers.txt', 'r') as password_file:
+        lines = password_file.readlines()
+
+
+def solve_part2():
+    with open('day7/answers.txt', 'r') as password_file:
+        lines = password_file.readlines()
---
+++ day8/day8.py
@@ -0,0 +1,8 @@
+def solve_part1():
+    with open('day8/answers.txt', 'r') as password_file:
+        lines = password_file.readlines()
+
+
+def solve_part2():
+    with open('day8/answers.txt', 'r') as password_file:
+        lines = password_file.readlines()
