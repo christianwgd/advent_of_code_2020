45d9518933b27fd52dafd87251439fbdc97a6d7a
Christian Wiegand <cwiegand@wgdnet.de>
2020-12-06 11:13:35 +0100

    day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,4 @@
-def solve():
+def solve_part2():
     with open('day6/answers.txt', 'r') as password_file:
         lines = password_file.readlines()
 
@@ -15,3 +15,23 @@ def solve():
             group.append(set(line.rstrip("\n")))
 
     return sum
+
+
+def solve_part1():
+    with open('day6/answers.txt', 'r') as password_file:
+        lines = password_file.readlines()
+
+    groups = []
+    group = []
+    product = 0
+    for line in lines:
+        if line == '\n':
+            groups.append(group)
+            product = product + len(set(group))
+            group = []
+
+        else:
+            for x in line.rstrip("\n"):
+                group.append(x)
+
+    return product
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from day6 import day6
 
 if __name__ == '__main__':
-    solution = day6.solve()
-    print('Lösung:', solution)
+    solution = day6.solve_part1()
+    print('Answer Part1:', solution)
+
+    solution = day6.solve_part2()
+    print('Answer Part2:', solution)
